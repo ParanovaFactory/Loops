6dd620bfb915ecc9c0fda7656b5606ea75310c98
ParanovaFactory <berkay_krdmn@hotmail.com>
2024-10-03 15:00:37 +0300

    Add project files.

---
+++ Loops/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num = int.Parse(Console.ReadLine());
+            //for (int i = 0; i <= num; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //int num = int.Parse(Console.ReadLine());
+            //int result = 0;
+            //for (int i = 0; i <= num; i++)
+            //{
+            //    result += i;
+            //}
+            //Console.WriteLine(result);
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //for (int i = 1; i <= 50; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int num = int.Parse(Console.ReadLine());
+            //int result = 1;
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    result *= i;
+            //}
+            //Console.WriteLine(result);
+
+            //int result = 0;
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 7 == 0)
+            //    {
+            //        result += i;
+            //    }
+            //}
+            //Console.WriteLine(result);
+
+            //int i = 1;
+            //while (i <= 50) 
+            //{
+            //    if (i % 8 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //    i++;
+            //}
+        }
+    }
+}
